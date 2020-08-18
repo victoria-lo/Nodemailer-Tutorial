1903e0b48df2496e5a2308cbb818cbd370331a1c
VicLo <victoria2666@gmail.com>
2020-08-18 16:50:42 -0400

    please

--- public/index.js
+++ public/index.js
@@ -2,7 +2,18 @@ const form = document.getElementById("contact-form");
 
 const formEvent = form.addEventListener("submit", (event) => {
   event.preventDefault();
-  let mail = new FormData(form);
+  let mail = new FormData();
+
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const subject = document.getElementById("subject").value;
+  const message = document.getElementById("message").value;
+
+  mail.append("name", name);
+  mail.append("email", email);
+  mail.append("subject", subject);
+  mail.append("message", message);
+
   sendMail(mail);
 });
 
