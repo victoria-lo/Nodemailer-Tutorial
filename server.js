692ac879507781f6b31d4701fc7fbe5aede7329e
VicLo <victoria2666@gmail.com>
2020-08-18 14:33:26 -0400

    heroku

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const app = express();
 // cors
 app.use(cors({ origin: "*" }));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 
 app.use("/public", express.static(process.cwd() + "/public")); //make public static
 
